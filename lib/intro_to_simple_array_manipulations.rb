7264837ba8a15ec5c1b4e5e5a4a5e44c25389e3c
Celeste Thomas <celestesmthomas@gmail.com>
2019-09-24 20:56:37 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,7 +3,74 @@ array.push(element)
 
 end
 
+
+
 def using_unshift(array, string)
   array.unshift("string")
   
 end
+
+
+
+def using_pop(array)
+  array.pop
+  
+end
+
+
+
+def pop_with_args
+  
+end 
+
+
+
+def using_shift
+  
+end
+
+
+
+def shift_with_args
+  
+end
+
+
+
+def using_concat
+  
+end
+
+
+
+def using_insert
+  
+end
+
+
+
+def using_uniq
+  
+end
+
+
+
+
+def using_flatten
+  
+end
+
+
+
+
+def using_delete
+  
+end
+
+
+
+
+def using_delete_at
+  
+end
+
